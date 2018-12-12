0dc28dba181a3c6cc061de34df3f4cbd118c40b5
wes14589 <36731762+wes14589@users.noreply.github.com>
2018-12-12 11:21:05 +0000

    Add files via upload

--- ACME_Interface/ACME_Interface/Program.cs
+++ ACME_Interface/ACME_Interface/Program.cs
@@ -30,36 +30,49 @@ namespace ACME_Interface
                 var contents = client.DownloadString("http://www.google.co.uk");
                 Console.WriteLine(contents);
                 Console.Read();
-            }
-
-
-
-            //using (IWebDriver driver = new FirefoxDriver())
-            //{
-            //    //Notice navigation is slightly different than the Java version
-            //    //This is because 'get' is a keyword in C#
-            //    driver.Navigate().GoToUrl("www.omdbapi.com/");
-
-            //    // Find the text input element by its name
-            //    IWebElement query = driver.FindElement(By.Name("q"));
-
-            //    // Enter something to search for
-            //    query.SendKeys("Cheese");
-
-            //    // Now submit the form. WebDriver will find the form for us from the element
-            //    query.Submit();
-
-            //    // Google's search is rendered dynamically with JavaScript.
-            //    // Wait for the page to load, timeout after 10 seconds
-            //    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            //    wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
-
-            //    // Should see: "Cheese - Google Search" (for an English locale)
-            //    Console.WriteLine("Page title is: " + driver.Title);
-            //}
-
-
+            };
         }
+                       
+        //public class OMovie // this is now situated in the main
+        //{
+        //    public string title;
+        //    public string year;
+        //    public string rated;
+        //    public string released;
+        //    public string runtime;
+        //    public string genre;
+        //    public string director;
+        //    public string poster;
 
+        //} 
+
+        //using (IWebDriver driver = new FirefoxDriver())
+        //{
+        //    //Notice navigation is slightly different than the Java version
+        //    //This is because 'get' is a keyword in C#
+        //    driver.Navigate().GoToUrl("www.omdbapi.com/");
+
+        //    // Find the text input element by its name
+        //    IWebElement query = driver.FindElement(By.Name("q"));
+
+        //    // Enter something to search for
+        //    query.SendKeys("Cheese");
+
+        //    // Now submit the form. WebDriver will find the form for us from the element
+        //    query.Submit();
+
+        //    // Google's search is rendered dynamically with JavaScript.
+        //    // Wait for the page to load, timeout after 10 seconds
+        //    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        //    wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+        //    // Should see: "Cheese - Google Search" (for an English locale)
+        //    Console.WriteLine("Page title is: " + driver.Title);
+        //    //}
+
+
+        //    //}
+        //}
     }
 }
+
